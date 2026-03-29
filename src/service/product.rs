7c199f8ee433c51e99c18b7c17ce004622d53349
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-29 19:40:09 +0700

    Edit product service methods to call notify after create/delete

--- src/service/product.rs
+++ src/service/product.rs
@@ -6,6 +6,7 @@ use crate::model::product::Product;
 use crate::repository::product::ProductRepository;
 use crate::service::notification::NotificationService;
 
+
 pub struct ProductService;
 
 impl ProductService {
@@ -13,6 +14,7 @@ impl ProductService {
         product.product_type = product.product_type.to_uppercase();
         let product_result: Product = ProductRepository::add(product);
 
+        NotificationService.notify(&product_result.product_type, "CREATED", product_result.clone());
         return Ok(product_result);
     }
 
@@ -39,7 +41,10 @@ impl ProductService {
                 String::from("Product not found.")
             ));
         }
+
         let product: Product = product_opt.unwrap();
+
+        NotificationService.notify(&product.product_type, "DELETED", product.clone());
 
         return Ok(Json::from(product));
     }
